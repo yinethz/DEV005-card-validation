9a179636c5a44fb25a02abf4dff5951b5ca7a2fd
yineth <nzambrano@sistemasenlinea.com.co>
2023-02-21 18:53:37 -0500

    feat: data validation was implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+function validarDatos() {
+
+  const number = document.getElementById("number").value;
+  const name = document.getElementById("name").value;
+  console.log(number, name);
+  const dataRecorded = document.getElementById("dataRecorded");
+  const numberMaskify = validator.maskify(number);
+  dataRecorded.innerText = "Hola " + name + " su número de tarjeta es: " + numberMaskify;
+
+}
+
+function limitNumber() {
+  const input = document.getElementById("number");
+  const number = input.value;
+  input.value = validator.limit(number);
+
+}
+
+window.limit = limitNumber;
+window.validar = validarDatos;
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
 const validator = {
-  // ...
+  maskify: function (word) {
+    if (word.length <= 4) {
+      return word;
+    }
+    return word.slice(0, -4).replace(/[0-9]/g, '#') + word.slice(-4);
+  },
+  limit: function (number) {
+    if (number.length > 16) {
+      return number.slice(0, 16);
+    }
+    return number;
+  }
 };
+
 
 export default validator;
